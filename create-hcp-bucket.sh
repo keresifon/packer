2197a01c476a8b728c1f9cec9f2ba273c37b9783
Kere Ekpenyong <Kere.Ekpenyong@ibm.com>
2025-12-01 23:00:25 -0500

    Clean up repository: remove redundant documentation files, add repository structure guide

--- create-hcp-bucket.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to create HCP Packer bucket via HCP CLI
-# Make sure you're authenticated: hcp auth login
-
-hcp packer buckets create ubuntu-golden-image \
-  --description "Ubuntu Golden Image for AWS" \
-  --labels os=ubuntu,managed-by=packer
-
